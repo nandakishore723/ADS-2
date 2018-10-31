7fb9e8255f05ab9d2e47de47c6350539496a71d7
nandakishore723 <nandakishore723@msitprogram.net>
2018-10-31 17:16:03 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignment/m2/2.1 CC_Percolation/Solution.java
+++ Assignment/m2/2.1 CC_Percolation/Solution.java
@@ -10,7 +10,7 @@ public final class Solution {
      * Constructs the object.
      */
     private Solution() {
-        //unused 
+        //unused
     }
     /**
      * main method to read input.
@@ -24,7 +24,7 @@ public final class Solution {
         while (scan.hasNext()) {
             String[] tokens = scan.nextLine().split(" ");
             pcObj.open(Integer.parseInt(tokens[0]),
-                      Integer.parseInt(tokens[1]));
+                       Integer.parseInt(tokens[1]));
         }
         System.out.println(pcObj.percolates()
                            && pcObj.numberOfOpenSites() != 0);

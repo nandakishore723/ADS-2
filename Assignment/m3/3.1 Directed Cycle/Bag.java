622f6df5fcf7a01b343198f1a01a74f9eff6f1ca
nandakishore723 <nandakishore723@msitprogram.net>
2018-11-01 17:31:47 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignment/m3/3.1 Directed Cycle/Bag.java
+++ Assignment/m3/3.1 Directed Cycle/Bag.java
@@ -121,7 +121,7 @@ public class Bag<Item> implements Iterable<Item> {
                 throw new NoSuchElementException();
             }
             Item item = current.item;
-            current= current.next;
+            current = current.next;
             return item;
         }
     }

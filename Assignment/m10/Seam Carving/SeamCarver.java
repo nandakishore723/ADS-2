ac9dd89c5f4a3b2ca8f5efdd1e5b8ca8e76dce51
nandakishore723 <nandakishore723@msitprogram.net>
2018-11-08 18:51:08 +0530

    "ads2_m10_a1_1: 5/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment/m10/Seam Carving/SeamCarver.java
+++ Assignment/m10/Seam Carving/SeamCarver.java
@@ -1,77 +1,77 @@
 import java.awt.Color;
 
 public class SeamCarver {
-	private Picture picture;
-	// create a seam carver object based on the given picture
-	public SeamCarver(Picture picture) {
-		if (picture == null) {
-			throw new IllegalArgumentException("picture is null");
-		}
-		this.picture = picture;
-	}
+    private Picture picture;
+    // create a seam carver object based on the given picture
+    public SeamCarver(Picture picture) {
+        if (picture == null) {
+            throw new IllegalArgumentException("picture is null");
+        }
+        this.picture = picture;
+    }
 
-	// current picture
-	public Picture picture() {
-		return picture;
-	}
+    // current picture
+    public Picture picture() {
+        return picture;
+    }
 
-	// width of current picture
-	public int width() {
-		return picture.width();
-	}
+    // width of current picture
+    public int width() {
+        return picture.width();
+    }
 
-	// height of current picture
-	public int height() {
-		return picture.height();
-	}
+    // height of current picture
+    public int height() {
+        return picture.height();
+    }
 
-	// energy of pixel at column x and row y
-	public double energy(int x, int y) {
-		if (x == 0 || y == 0 || x == picture.width() - 1 || y == picture.height() - 1) {
-			return 1000;
-		} else {
-			Color left = picture.get(x, y - 1);
-			Color right = picture.get(x, y + 1);
-			Color top = picture.get(x - 1, y);
-			Color bottom = picture.get(x + 1, y);
-			int rh = bottom.getRed() - top.getRed();
-			int bh = bottom.getBlue() - top.getBlue();
-			int gh = bottom.getGreen() - top.getGreen();
-			int horizontal = rh * rh + bh * bh + gh * gh;
-			int rv = left.getRed() - right.getRed();
-			int bv = left.getBlue() - right.getBlue();
-			int gv = left.getGreen() - right.getGreen();
-			int vertical = rv * rv + bv * bv + gv * gv;
-			double enr = Math.sqrt(horizontal + vertical);
-			return enr;
-		}
-	}
+    // energy of pixel at column x and row y
+    public double energy(int x, int y) {
+        if (x == 0 || y == 0 || x == picture.width() - 1 || y == picture.height() - 1) {
+            return 1000;
+        } else {
+            Color left = picture.get(x, y - 1);
+            Color right = picture.get(x, y + 1);
+            Color top = picture.get(x - 1, y);
+            Color bottom = picture.get(x + 1, y);
+            int rh = bottom.getRed() - top.getRed();
+            int bh = bottom.getBlue() - top.getBlue();
+            int gh = bottom.getGreen() - top.getGreen();
+            int horizontal = rh * rh + bh * bh + gh * gh;
+            int rv = left.getRed() - right.getRed();
+            int bv = left.getBlue() - right.getBlue();
+            int gv = left.getGreen() - right.getGreen();
+            int vertical = rv * rv + bv * bv + gv * gv;
+            double enr = Math.sqrt(horizontal + vertical);
+            return enr;
+        }
+    }
 
-	/*private void calculateEnergies() {
-		for (int i = 0; i < this.width(); i++) {
-			for (int j = 0; j < this.height(); j++) {
-				energies[i][j] = energy(i, j);
-			}
-		}
-	}*/
+    /*private void calculateEnergies() {
+        for (int i = 0; i < this.width(); i++) {
+            for (int j = 0; j < this.height(); j++) {
+                energies[i][j] = energy(i, j);
+            }
+        }
+    }*/
 
-	// sequence of indices for horizontal seam
-	public int[] findHorizontalSeam() {
-		return new int[0];
-	}
+    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        return new int[0];
+    }
 
-	// sequence of indices for vertical seam
-	public int[] findVerticalSeam() {
-		return new int[0];
-	}
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        return new int[0];
+    }
 
-	// remove horizontal seam from current picture
-	public void removeHorizontalSeam(int[] seam) {
+    // remove horizontal seam from current picture
+    public void removeHorizontalSeam(int[] seam) {
 
-	}
+    }
 
-	// remove vertical seam from current picture
-	public void removeVerticalSeam(int[] seam) {
+    // remove vertical seam from current picture
+    public void removeVerticalSeam(int[] seam) {
 
-	}
+    }
 }

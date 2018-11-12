9d33bc3170680df77726d30eccf16c473262007a
nandakishore723 <nandakishore723@msitprogram.net>
2018-11-12 15:08:01 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment/m13/Radix Sorts/Solution.java
+++ Assignment/m13/Radix Sorts/Solution.java
@@ -5,24 +5,32 @@ import java.util.Scanner;
 
 public final class Solution {
 
-	private Solution() {
-		//unused
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused
+    }
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Quick3string q3s = new Quick3string();
-		int n = Integer.parseInt(scan.nextLine());
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < n; i++) {
-			sb.append(scan.nextLine() + "::");
-		}
-		String[] lines = sb.toString().split("::");
-		q3s.sort(lines);
-		String str = "[";
-		for (int i = 0; i < n; i++) {
-			str += lines[i] + ", ";
-		}
-		System.out.println(str.substring(0, str.length() - 2) + "]");
-	}
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Quick3string q3s = new Quick3string();
+        int n = Integer.parseInt(scan.nextLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(scan.nextLine() + "::");
+        }
+        String[] lines = sb.toString().split("::");
+        q3s.sort(lines);
+        String str = "[";
+        for (int i = 0; i < n; i++) {
+            str += lines[i] + ", ";
+        }
+        System.out.println(str.substring(0, str.length() - 2) + "]");
+    }
 }

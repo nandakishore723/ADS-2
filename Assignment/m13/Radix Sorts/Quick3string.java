d2f4a320127f1ba6335ec2723db6c33ef2e8cf5a
nandakishore723 <nandakishore723@msitprogram.net>
2018-11-12 15:27:21 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment/m13/Radix Sorts/Quick3string.java
+++ Assignment/m13/Radix Sorts/Quick3string.java
@@ -10,33 +10,53 @@
  *  @author Kevin Wayne
  */
 
-public class Quick3string {
+public final class Quick3string {
     private static final int CUTOFF =  15;   // cutoff to insertion sort
 
-    // do not instantiate
-    Quick3string() { }
+    //
+    // do not instantiate.
+    //
+    Quick3string() {
+        //unused
+    }
 
     /**
      * Rearranges the array of strings in ascending order.
      *
-     * @param a the array to be sorted
+     * @param      a     the array to be sorted.
      */
-    public static void sort(String[] a) {
+    public static void sort(final String[] a) {
         StdRandom.shuffle(a);
         sort(a, 0, a.length - 1, 0);
         assert isSorted(a);
     }
 
+    //
     // return the dth character of s, -1 if d = length of s
-    private static int charAt(String s, int d) {
+    //
+    // @param      s     { parameter_description }.
+    // @param      d     { parameter_description }.
+    //
+    // @return     { description_of_the_return_value }.
+    //
+    private static int charAt(final String s, final int d) {
         assert d >= 0 && d <= s.length();
-        if (d == s.length()) return -1;
+        if (d == s.length()) {
+            return -1;
+        }
         return s.charAt(d);
     }
 
 
+    //
     // 3-way string quicksort a[lo..hi] starting at dth character
-    private static void sort(String[] a, int lo, int hi, int d) {
+    //
+    // @param      a     { parameter_description }.
+    // @param      lo    The lower.
+    // @param      hi    The higher.
+    // @param      d     { parameter_description }.
+    //
+    private static void sort(final String[] a, final int lo, final int hi, final int d) {
 
         // cutoff to insertion sort for small subarrays
         if (hi <= lo + CUTOFF) {
@@ -49,9 +69,13 @@ public class Quick3string {
         int i = lo + 1;
         while (i <= gt) {
             int t = charAt(a[i], d);
-            if      (t < v) exch(a, lt++, i++);
-            else if (t > v) exch(a, i, gt--);
-            else              i++;
+            if      (t < v) {
+                exch(a, lt++, i++);
+            } else if (t > v) {
+                exch(a, i, gt--);
+            } else {
+                i++;
+            }
         }
 
         // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi].
@@ -60,8 +84,15 @@ public class Quick3string {
         sort(a, gt + 1, hi, d);
     }
 
+    //
     // sort from a[lo] to a[hi], starting at the dth character
-    private static void insertion(String[] a, int lo, int hi, int d) {
+    //
+    // @param      a     { parameter_description }.
+    // @param      lo    The lower.
+    // @param      hi    The higher.
+    // @param      d     { parameter_description }.
+    //
+    private static void insertion(final String[] a, final int lo, final int hi, final int d) {
         for (int i = lo; i <= hi; i++) {
             for (int j = i; j > lo && less(a[j], a[j - 1], d); j--) {
                 exch(a, j, j - 1);
@@ -69,8 +100,14 @@ public class Quick3string {
         }
     }
 
+    //
     // exchange a[i] and a[j]
-    private static void exch(String[] a, int i, int j) {
+    //
+    // @param      a     { parameter_description }.
+    // @param      i     { parameter_description }.
+    // @param      j     { parameter_description }.
+    //
+    private static void exch(final String[] a, final int i, final int j) {
         String temp = a[i];
         a[i] = a[j];
         a[j] = temp;
@@ -83,20 +120,41 @@ public class Quick3string {
     //    return v.substring(d).compareTo(w.substring(d)) < 0;
     // }
 
+    //
     // is v less than w, starting at character d
-    private static boolean less(String v, String w, int d) {
+    //
+    // @param      v     { parameter_description }.
+    // @param      w     { parameter_description }.
+    // @param      d     { parameter_description }.
+    //
+    // @return     { description_of_the_return_value }.
+    //
+    private static boolean less(final String v, final String w, final int d) {
         assert v.substring(0, d).equals(w.substring(0, d));
         for (int i = d; i < Math.min(v.length(), w.length()); i++) {
-            if (v.charAt(i) < w.charAt(i)) return true;
-            if (v.charAt(i) > w.charAt(i)) return false;
+            if (v.charAt(i) < w.charAt(i)) {
+                return true;
+            }
+            if (v.charAt(i) > w.charAt(i)) {
+                return false;
+            }
         }
         return v.length() < w.length();
     }
 
+    //
     // is the array sorted
-    private static boolean isSorted(String[] a) {
-        for (int i = 1; i < a.length; i++)
-            if (a[i].compareTo(a[i - 1]) < 0) return false;
+    //
+    // @param      a     { parameter_description }.
+    //
+    // @return     True if sorted, False otherwise.
+    //
+    private static boolean isSorted(final String[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (a[i].compareTo(a[i - 1]) < 0) {
+                return false;
+            }
+        }
         return true;
     }
 

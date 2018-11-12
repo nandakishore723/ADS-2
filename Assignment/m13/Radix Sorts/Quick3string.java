5613692fe67d2a9f4c3979f928eadda5681cf817
nandakishore723 <nandakishore723@msitprogram.net>
2018-11-12 16:02:07 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignment/m13/Radix Sorts/Quick3string.java
+++ Assignment/m13/Radix Sorts/Quick3string.java
@@ -29,19 +29,20 @@ public final class Quick3string {
      * @param      a     the array to be sorted.
      */
     public static void sort(final String[] a) {
-        StdRandom.shuffle(a);
+        //StdRandom.shuffle(a);
         sort(a, 0, a.length - 1, 0);
         assert isSorted(a);
     }
 
-    //
-    // return the dth character of s, -1 if d = length of s
-    //
-    // @param      s     { parameter_description }.
-    // @param      d     { parameter_description }.
-    //
-    // @return     { description_of_the_return_value }.
-    //
+
+    /**
+     * { function_description }.
+     *
+     * @param      s     { parameter_description }.
+     * @param      d     { parameter_description }.
+     *
+     * @return     { description_of_the_return_value }.
+     */
     private static int charAt(final String s, final int d) {
         assert d >= 0 && d <= s.length();
         if (d == s.length()) {
@@ -50,18 +51,20 @@ public final class Quick3string {
         return s.charAt(d);
     }
 
+    /**
+     * { function_description }.
+     *
+     * @param      a     { parameter_description }.
+     * @param      lo    The lower.
+     * @param      hi    The higher.
+     * @param      d     { parameter_description }.
+     */
+    private static void sort(final String[] a, final int lo, final int hi,
+                             final int d) {
 
-    //
-    // 3-way string quicksort a[lo..hi] starting at dth character
-    //
-    // @param      a     { parameter_description }.
-    // @param      lo    The lower.
-    // @param      hi    The higher.
-    // @param      d     { parameter_description }.
-    //
-    private static void sort(final String[] a, final int lo, final int hi, final int d) {
-
-        // cutoff to insertion sort for small subarrays
+        /**
+        * cutoff to insertion sort for small subarrays
+        */
         if (hi <= lo + CUTOFF) {
             insertion(a, lo, hi, d);
             return;
@@ -81,21 +84,24 @@ public final class Quick3string {
             }
         }
 
-        // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi].
+        /**
+        * a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi].
+        */
         sort(a, lo, lt - 1, d);
         if (v >= 0) sort(a, lt, gt, d + 1);
         sort(a, gt + 1, hi, d);
     }
 
-    //
-    // sort from a[lo] to a[hi], starting at the dth character
-    //
-    // @param      a     { parameter_description }.
-    // @param      lo    The lower.
-    // @param      hi    The higher.
-    // @param      d     { parameter_description }.
-    //
-    private static void insertion(final String[] a, final int lo, final int hi, final int d) {
+    /**
+     * { function_description }.
+     *
+     * @param      a     { parameter_description }.
+     * @param      lo    The lower.
+     * @param      hi    The higher.
+     * @param      d     { parameter_description }.
+     */
+    private static void insertion(final String[] a, final int lo, final int hi,
+                                  final int d) {
         for (int i = lo; i <= hi; i++) {
             for (int j = i; j > lo && less(a[j], a[j - 1], d); j--) {
                 exch(a, j, j - 1);
@@ -103,13 +109,14 @@ public final class Quick3string {
         }
     }
 
-    //
-    // exchange a[i] and a[j]
-    //
-    // @param      a     { parameter_description }.
-    // @param      i     { parameter_description }.
-    // @param      j     { parameter_description }.
-    //
+
+    /**
+     * { function_description }.
+     *
+     * @param      a     { parameter_description }.
+     * @param      i     { parameter_description }.
+     * @param      j     { parameter_description }.
+     */
     private static void exch(final String[] a, final int i, final int j) {
         String temp = a[i];
         a[i] = a[j];
@@ -123,15 +130,15 @@ public final class Quick3string {
     //    return v.substring(d).compareTo(w.substring(d)) < 0;
     // }
 
-    //
-    // is v less than w, starting at character d
-    //
-    // @param      v     { parameter_description }.
-    // @param      w     { parameter_description }.
-    // @param      d     { parameter_description }.
-    //
-    // @return     { description_of_the_return_value }.
-    //
+    /**
+     * { function_description }.
+     *
+     * @param      v     { parameter_description }.
+     * @param      w     { parameter_description }.
+     * @param      d     { parameter_description }.
+     *
+     * @return     { description_of_the_return_value }.
+     */
     private static boolean less(final String v, final String w, final int d) {
         assert v.substring(0, d).equals(w.substring(0, d));
         for (int i = d; i < Math.min(v.length(), w.length()); i++) {
@@ -145,13 +152,13 @@ public final class Quick3string {
         return v.length() < w.length();
     }
 
-    //
-    // is the array sorted
-    //
-    // @param      a     { parameter_description }.
-    //
-    // @return     True if sorted, False otherwise.
-    //
+    /**
+     * Determines if sorted.
+     *
+     * @param      a     { parameter_description }.
+     *
+     * @return     True if sorted, False otherwise.
+     */
     private static boolean isSorted(final String[] a) {
         for (int i = 1; i < a.length; i++) {
             if (a[i].compareTo(a[i - 1]) < 0) {
